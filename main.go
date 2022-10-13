6448812b74599272c0c8f9290a41a5d9b125a37f
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2022-10-13 23:08:14 +0100

    fixing json parse error

--- main.go
+++ main.go
@@ -9,39 +9,22 @@ import (
 )
 
 type SteamJson struct{
-	headers []string
-	data [][]float64
+	Headers []string `json:"headers"`
+	Data [][]float64 `json:"data"`
 }
 
-var steamJson *SteamJson
+
+var steamJson SteamJson
 func main() {
-	file, err := os.Open("./tables/saturated_by_pressure_V1.3.json")
+	file, err := os.Open("./tables/saturated_by_pressure.json")
 
 	if err != nil {
 		log.Fatalf("Error opening file: %v", err)
 	}
 	defer file.Close()
 	byteValue, _ := io.ReadAll(file)
+
 	json.Unmarshal(byteValue, &steamJson)
-	
+
+	fmt.Println(steamJson.Headers)
 }
-
-// func getPropsByPressure_sat(pressureValue float64) int {
-// 	file, err := os.Open("./tables/saturated_by_pressure_V1.3.json")
-
-// 	if err != nil {
-// 		log.Fatalf("Error opening file: %v", err)
-// 	}
-// 	defer file.Close()
-// 	byteValue, _ := io.ReadAll(file)
-// 	json.Unmarshal(byteValue, &steamJson)
-
-// 	for i := 0; i < len(steamJson.data); i++ {
-// 		for j := 0; j < len(steamJson.headers); j++ {
-// 			if (steamJson.data[i])[j] == float32(pressureValue) {
-// 				return len(steamJson.data)
-// 			}
-// 		}
-// 	}
-// 	return 1
-// }
