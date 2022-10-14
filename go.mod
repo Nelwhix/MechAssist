f802f9365b1bea13329bfea1c3ce8f44bd96ec53
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2022-10-14 01:53:52 +0100

    feat: can solve for rankine cycle

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/Nelwhix/mechAssist
 
 go 1.19
+
+// require (
+// 	github.com/samber/lo v1.32.0 // indirect
+// 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+// )
+
+require (
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"github.com/olekukonko/tablewriter"
 )
 
 type SteamJson struct{
@@ -16,6 +17,57 @@ type SteamJson struct{
 
 var steamJson SteamJson
 func main() {
+	// units must be in MPa
+	newRankine(0.01, 2)
+}
+
+func newRankine(pressureCondenser float64, pressureBoiler float64)  {
+	var wp float64 // Work done at the pump
+	var qh float64 // Heat added at the boiler
+	var wt float64 // Work done at the turbine
+	var x4 float64 // dryness fraction of steam when it leaves the boiler
+
+	// At the pump
+	v := getSteamPropsByPressure_sat(pressureCondenser)[13]
+	wp = (v * (pressureBoiler - pressureCondenser)) * 1000
+	h1 := getSteamPropsByPressure_sat(pressureCondenser)[7]
+	h2 := h1 + wp
+	
+	// Boiler
+	h3 := getSteamPropsByPressure_sat(pressureBoiler)[8]
+	qh = h3 - h2
+
+	// Turbine
+	x4 = (getSteamPropsByPressure_sat(pressureBoiler)[11] - getSteamPropsByPressure_sat(pressureCondenser)[10]) / getSteamPropsByPressure_sat(pressureCondenser)[12]
+	h4 := h1  + x4 * getSteamPropsByPressure_sat(pressureCondenser)[9]
+
+	wt = h3 - h4
+
+	// Condenser
+	ql := h4 - h1
+
+	// Efficiency of cycle
+	nth := ((wt - wp)/ qh) * 100
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Process", "Inlet State", "Exit State", "Work done/Heat transfer"})
+	data := [][]string{
+		{"Pump", "P1 = " + strconv.FormatFloat(pressureCondenser, 'g', 5, 64) + "MPa", "h2 = " + strconv.FormatFloat(h2, 'g', 5, 64) + "MPa", "Work Input: " + strconv.FormatFloat(wp, 'g', 5, 64) + "KJ/KG"},
+		{"Boiler", "P2 = " + strconv.FormatFloat(pressureBoiler, 'g', 5, 64) + "MPa", "No output", "Heat Added : " + strconv.FormatFloat(qh, 'g', 5, 64) + "KJ/KG"},
+		{"Turbine", "State 3 known", "x4 = " + strconv.FormatFloat(x4, 'g', 5, 64) + "MPa " + "h4 = " + strconv.FormatFloat(h4, 'g', 5, 64) , "Work output : " + strconv.FormatFloat(wt, 'g', 5, 64) + "KJ/KG"},
+		{"Condenser", "State 4 known", "Output known", "Heat rejected : " + strconv.FormatFloat(ql, 'g', 5, 64) + "KJ/KG"},
+	}
+
+	for _, v := range data {
+		table.Append(v)
+	}
+	table.SetFooter([]string{"", "", "Efficiency of Cycle", strconv.FormatFloat(nth, 'g', 5, 64) + "%"})
+
+	table.Render()
+
+}
+
+func getSteamPropsByPressure_sat(pressureValue float64) []float64 {
 	file, err := os.Open("./tables/saturated_by_pressure.json")
 
 	if err != nil {
@@ -26,5 +78,11 @@ func main() {
 
 	json.Unmarshal(byteValue, &steamJson)
 
-	fmt.Println(steamJson.Headers)
+	for i := 0; i < len(steamJson.Data); i++ {
+		if pressureValue == steamJson.Data[i][0] {
+			return steamJson.Data[i]
+		}
+	}
+
+	return nil
 }
